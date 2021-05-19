c25628c53706cb39289c90d8d7a218067a2d45ce
Somendra <somendra.pratapsingh.15@gmail.com>
2021-05-20 01:50:19 +0530

    Day 1

---
+++ cpp/init/1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int a,b;
+
+    cin>>a>>b;
+    cout<<a+b<<endl;
+
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(bool)<<endl;
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(long int)<<endl;
+    cout<<sizeof(short int)<<endl;
+    cout<<sizeof(signed int)<<endl;
+    cout<<sizeof(unsigned int)<<endl;
+
+    return 0;
+}
